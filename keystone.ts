1f988bc749589dd3452acbc9444104e4b3bcaf1d
Taylor Covington <tcovingtondev@gmail.com>
2021-02-13 21:41:21 -0700

    keystone in a working state

--- keystone.ts
+++ keystone.ts
@@ -10,7 +10,7 @@ const databaseURL = process.env.DATABASE_URL;
 const sessionConfig = {
   // how long does user stay signed in?
   maxAge: 60 * 60 * 24 * 360,
-  secret: process.env.CCOOKIE_SECRET
+  secret: process.env.COOKIE_SECRET
 };
 
 const { withAuth } = createAuth({
@@ -18,7 +18,7 @@ const { withAuth } = createAuth({
   identityField: 'email',
   secretField: 'password',
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ['firstName', 'lastName', 'email', 'password'],
     // TODO add in initial roles here
   }
 })
@@ -48,6 +48,6 @@ export default withAuth(config({
     },
   },
   session: withItemData(statelessSessions(sessionConfig), {
-    User: 'id',
+    User: `id`,
   })
 }));
